5a13631589381b631eb81d97f14928fc9e6192aa
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-08-24 08:46:41 -0700

    Complete binary search

--- practice.rb
+++ practice.rb
@@ -1,13 +1,15 @@
 class Array 
   
-  def binary_search(target   )
+  def binary_search(target)
     mid = count/2
     return mid if self[mid] == target
-    
+    lower = self.take(mid)
+    upper = self.drop(mid)
     if self[mid] < target
-      result = binar
+      result = binary_search(upper, target)
+      result.nil? ? result : result + mid
     else
-      
+      binary_search(lower, target)
     end 
   end 
   
