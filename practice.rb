77e62b94661bd9ffeb66909ec85677c3ed60388b
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-08-24 08:42:24 -0700

    Add base case for binary search

---
+++ practice.rb
@@ -0,0 +1,18 @@
+class Array 
+  
+  def binary_search(target   )
+    mid = count/2
+    return mid if self[mid] == target
+    
+    if self[mid] < target
+      result = binar
+    else
+      
+    end 
+  end 
+  
+end 
+
+def fib_rec(n)
+  
+end 
